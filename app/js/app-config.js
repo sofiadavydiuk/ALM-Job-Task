1d95e3b53c6c31b27703086aab920b57f92aef02
sofilime <sofia.davudyuk@bright-it.com>
2018-02-09 19:55:02 +0100

    Added: main admin page added

--- app/js/app-config.js
+++ app/js/app-config.js
@@ -1,21 +1,24 @@
-mainApp.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
-	$routeProvider
-	.when("/", {
-		templateUrl : "/view/main.html"
-	})
-	.when("/contacts", {
-		templateUrl : "/view/contacts.html"
-	})
-	.when("/product", {
-		templateUrl : "/view/product.html"
-	})
-	.otherwise({
-		templateUrl : "/view/404.html"
-	});
+mainApp.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when("/", {
+            templateUrl: "/view/main.html"
+        })
+        .when("/contacts", {
+            templateUrl: "/view/contacts.html"
+        })
+        .when("/product", {
+            templateUrl: "/view/product.html"
+        })
+        .when("/admin", {
+            templateUrl: "/view/admin.html"
+        })
+        .otherwise({
+            templateUrl: "/view/404.html"
+        });
 
-	$locationProvider.html5Mode({
-		enabled: true,
-		requireBase: false
-	});
+    $locationProvider.html5Mode({
+        enabled: true,
+        requireBase: false
+    });
 
 }]);
